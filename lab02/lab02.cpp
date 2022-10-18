a9c24f49402e3b663735a75eba0274ed9daa4946
mishhgun01 <AVATARno15>
2022-10-18 16:18:19 +0300

    classes

--- lab02/lab02.cpp
+++ lab02/lab02.cpp
@@ -6,14 +6,10 @@
 #include "Set.h"
 using namespace std;
 int Set::N = 26; // определение статического члена класса
-const long q0 = 100000; // количество повторений цикла времени
 int main()
 { srand(time(nullptr));
     class Set A('A'), B('B'), C('C'), D('D'), E;
-    for(long q =0; q < q0; q++)
-    { E = (A | B) & (C & ~D); }
-    cout << " Middle power =" <<
-         (A.power() + B.power() + C.power() + D.power() + E.power()) / 5
-         << q0 << endl;
+    E = A&~(B|C|D);
+    E.Show();
     return 0;
 }
